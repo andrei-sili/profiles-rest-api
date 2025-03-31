726cab38e9d048645e9feff5c7ea7e996fb45029
Andrei <andrei.sili1985@gmail.com>
2025-03-31 10:51:34 +0200

    Added profiles viewset

--- profiles_api/serializers.py
+++ profiles_api/serializers.py
@@ -1,6 +1,40 @@
 from rest_framework import serializers
+from profiles_api import models
 
 
 class HelloSerializer(serializers.Serializer):
     """Serializes a name field for testing our APIView"""
     name = serializers.CharField(max_length=10)
+
+
+class UserProfileSerializer(serializers.ModelSerializer):
+    """Serializează un obiect de tip profil de utilizator"""
+
+    class Meta:
+        model = models.UserProfile
+        fields = ('id', 'email', 'name', 'password')
+        extra_kwargs = {
+            'password': {
+                'write_only': True,
+                'style': {'input_type': 'password'}
+            }
+        }
+
+    def create(self, validated_data):
+        """Creează și returnează un nou utilizator"""
+        user = models.UserProfile.objects.create_user(
+            email=validated_data['email'],
+            name=validated_data['name'],
+            password=validated_data['password']
+        )
+        return user
+
+    def update(self, instance, validated_data):
+        new_password = validated_data.pop('password', None)
+        user = super().update(instance, validated_data)
+
+        if new_password:
+            user.set_password(new_password)
+            user.save()
+
+        return user
